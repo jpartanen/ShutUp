a57ea98368cd2494118e924d01b4f124e9dcb478
j partanen <greytama@yahoo.com>
2012-01-07 10:41:09 +0200

    add first event handler

--- src/java/android/shutup/ShutUpActivity.java
+++ src/java/android/shutup/ShutUpActivity.java
@@ -2,6 +2,7 @@ package java.android.shutup;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class ShutUpActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +10,17 @@ public class ShutUpActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        setButtonClickListener();
+        
+    }
+    private void setButtonClickListener(){
+    	
+        Button toggleButton = (Button)findViewById(R.id.toggleButton);
+        toggleButton.setOnClickListener(new view.OnClickListener(){
+    	public void onClick(View v){
+    		
+    	}
+        });
     }
 }
